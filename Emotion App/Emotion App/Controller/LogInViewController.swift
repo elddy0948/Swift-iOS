38e1b9b7810af7e26be96e9cfac6695c2b20a75a
김호준 <elddy0948@hotmail.co.kr>
2020-07-17 20:18:20 +0900

    Add Date Picker and UI for SignUp and LogIn

--- Emotion App/Emotion App/Controller/LogInViewController.swift
+++ Emotion App/Emotion App/Controller/LogInViewController.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        Utilities.styleButton(loginButton)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Emotion App/Emotion App/Controller/SignUpViewController.swift
+++ Emotion App/Emotion App/Controller/SignUpViewController.swift
@@ -9,22 +9,42 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var lastnameTextField: UITextField! //
+    @IBOutlet weak var firstnameTextField: UITextField! //이름
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var birthDatePicker: UIDatePicker!
+    
+    var lastname = ""
+    var firstname = ""
+    var email = ""
+    var password = ""
+    var birth = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        Utilities.styleButton(signupButton)
+    }
+    func cleanedTextField() {
+        lastname = lastnameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        firstname = firstnameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+    }
+    @IBAction func birthdateChanged(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        birth = dateFormatter.string(from: birthDatePicker.date)
+    }
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        cleanedTextField()
+        print("lastname : \(lastname) \n firstname : \(firstname) \n email : \(email) \n password : \(password) \n birthdate : \(birth)" )
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
