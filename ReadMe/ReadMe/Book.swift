a1ff4ed1b581031f7f3d35c8dc87bf73911211e0
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-09-10 20:51:29 +0900

    2020-09-10

--- ReadMe/ReadMe/Book.swift
+++ ReadMe/ReadMe/Book.swift
@@ -1,9 +1,10 @@
 import UIKit
 
-struct Book {
+struct Book: Hashable {
     let title: String
     let author: String
     var review: String?
+    var readMe: Bool
     
     var image: UIImage {
         Library.loadImage(forBook: self) ?? LibrarySymbol.letterSquare(letter: title.first).image
