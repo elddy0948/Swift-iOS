282adabd8ec7ef4c73939337cb96137ac0b00e13
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-09-11 18:58:25 +0900

    Collection View Practice 01

---
+++ Collection View Practice/Collection View Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection View Practice
+//
+//  Created by 김호준 on 2020/09/11.
+//  Copyright © 2020 김호준. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
