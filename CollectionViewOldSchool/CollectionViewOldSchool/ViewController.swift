79c8d5efeb62fa2837184d85f16264326160f0fa
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-09-15 22:05:20 +0900

    Collection View Old School

---
+++ CollectionViewOldSchool/CollectionViewOldSchool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewOldSchool
+//
+//  Created by 김호준 on 2020/09/15.
+//  Copyright © 2020 김호준. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
