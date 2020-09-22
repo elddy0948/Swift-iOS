a2a77e0bd1df4beb15757e9ba3d0909d6eaa0d5b
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-09-22 22:21:35 +0900

    Data pass with Segue

--- Collection View Old School/Collection View Old School/ViewController.swift
+++ Collection View Old School/Collection View Old School/ViewController.swift
@@ -20,7 +20,18 @@ class ViewController: UIViewController {
         collectionView.dataSource = dataSource
         collectionView.delegate = delegate
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "showEmojiDetail",
+              let emojiCell = sender as? EmojiCell,
+              let emojiDetailController = segue.destination as? EmojiDetailController,
+              let indexPath = collectionView.indexPath(for: emojiCell),
+              let emoji = Emoji.shared.emoji(at: indexPath)
+        else {
+            fatalError("Can't go Emoji Detail Controller")
+        }
+        
+        emojiDetailController.emoji = emoji
+    }
 }
 
