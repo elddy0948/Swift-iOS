001aadf9d604a7fa57ab69fcfb378ade2ee62c50
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-10-01 23:16:13 +0900

    instagram clone fix bugs

--- Instagram Clone/Instagram Clone/Controllers/Core Tabs/NotificationViewController.swift
+++ Instagram Clone/Instagram Clone/Controllers/Core Tabs/NotificationViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class NotificationViewController: UIViewController {
     
+    private let tableView: UITableView = {
+        let table = UITableView()
+        
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        title = "Notification"
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    override func viewDidLayoutSubviews() {
+        tableView.frame = view.bounds
+    }
+    
+}
+
+extension NotificationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
 }
--- Instagram Clone/Instagram Clone/Controllers/Other/General/SettingsViewController.swift
+++ Instagram Clone/Instagram Clone/Controllers/Other/General/SettingsViewController.swift
@@ -52,20 +52,31 @@ class SettingsViewController: UIViewController {
         data.append(section)
     }
     private func didTapLogOut() {
-        AuthManager.shared.logout { (success) in
-            DispatchQueue.main.async {
-                if success {
-                    let loginVC = LoginViewController()
-                    loginVC.modalPresentationStyle = .fullScreen
-                    self.present(loginVC, animated: true) {
-                        self.navigationController?.popToRootViewController(animated: false)
-                        self.tabBarController?.selectedIndex = 0
+        
+        let actionSheet = UIAlertController(title: "Log Out",
+                                            message: "Are you sure you want to Log Out?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive, handler: { _ in
+            AuthManager.shared.logout { (success) in
+                DispatchQueue.main.async {
+                    if success {
+                        let loginVC = LoginViewController()
+                        loginVC.modalPresentationStyle = .fullScreen
+                        self.present(loginVC, animated: true) {
+                            self.navigationController?.popToRootViewController(animated: false)
+                            self.tabBarController?.selectedIndex = 0
+                        }
+                    } else {
+                        fatalError("Could not Log Out User")
                     }
-                } else {
-                    
                 }
             }
-        }
+        }))
+        //For iPad
+        actionSheet.popoverPresentationController?.sourceView = tableView
+        actionSheet.popoverPresentationController?.sourceRect = tableView.bounds
+        
+        present(actionSheet, animated: true, completion: nil)
     }
 }
 
