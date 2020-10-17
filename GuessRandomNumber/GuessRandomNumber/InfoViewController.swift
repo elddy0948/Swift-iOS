836519e359a50990f0507a930e62e285a5c3cd95
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-10-18 01:43:52 +0900

    end guessRandomNumber

---
+++ GuessRandomNumber/GuessRandomNumber/InfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InfoViewController.swift
+//  GuessRandomNumber
+//
+//  Created by 김호준 on 2020/10/18.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
